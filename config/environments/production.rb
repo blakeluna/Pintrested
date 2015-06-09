91cb87a26a433212460cfd9f65db8e6f427c1206
Blake Luna <blakeluna@gmail.com>
2015-06-09 01:49:47 +0000

    Heroku Update for Amazon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ config.action_mailer.default_url_options = {host:'https://pinterested1011.heroku
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-  :bucket => ENV['S#_BUCKET_NAME'], 
+  :bucket => ENV['S3_BUCKET_NAME'], 
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
