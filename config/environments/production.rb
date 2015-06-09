46f1a58352de390e5cd45aa9856f355e0ab685bf
Blake Luna <blakeluna@gmail.com>
2015-06-09 01:29:29 +0000

    Heroku S3 setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ config.action_mailer.default_url_options = {host:'https://pinterested1011.heroku
 config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-  :bucket => ENV['S3_BUCKET_NAME'],
-  :access_key_id => ENV['AWS_Access_KEY_ID'],
+  :bucket => ENV['S#_BUCKET_NAME'], 
+  :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 }
